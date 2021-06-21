ef86be491327b600e3f53af922dec9f74177f8e5
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-06-20 21:26:26 -0400

    i'm getting an error I do not understand and it's making it impossinle to complete this project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider} from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-import reducer from './reducers';
+import reducer from "./reducers/movieReducer";
 
-import App from './App'
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+import App from "./App";
+// import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+
+const store = createStore(reducer);
+
+const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-  <Router>
-      <App />
-  </Router>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
 );
